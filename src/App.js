ff3183bbb824dfa2703188773a2f3100b69bdd1e
mgraphic <mgraphic@users.noreply.github.com>
2022-12-17 12:57:00 -0500

    create mock data

--- src/App.js
+++ src/App.js
@@ -1,35 +1,3 @@
-// import { Fragment, useState } from 'react';
-// import { Button } from 'react-bootstrap';
-import { Dashboard } from './components/dashboard/Dashboard';
-// import { AsyncExample } from './AsyncExample';
-// import { ModalExample } from './ModalExample';
+import { Dashboard } from "./components/dashboard/Dashboard";
 
-export const App = () => {
-    /*
-    // Bootstrap Lookahead example
-    const handleSelected = (selected) => {
-        console.log(`FROM App.js => You selected: ${selected.label}`, selected);
-    };
-
-    return <AsyncExample handleSelected={handleSelected} />;
-    */
-    /* // Bootstrap Modal example
-    const [show, setShow] = useState(false);
-
-    return (
-        <>
-            <Button variant="primary" onClick={() => setShow(true)}>
-                Show modal
-            </Button>
-
-    {/ * {show ? <ModalExample show={show} setShow={setShow} /> : ''} * /}
-            <ModalExample show={show} setShow={setShow} />
-    {/ * {show ? 'true' : 'false'} * /}
-        </>
-    ); */
-
-    return <Dashboard />;
-};
-
-// export default App;
-// crypto.randomUUID()
+export const App = () => <Dashboard />;
